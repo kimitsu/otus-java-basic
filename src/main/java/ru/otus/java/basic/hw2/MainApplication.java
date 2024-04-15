55bb0f837c0331a999316f310f6e5268b384d570
kimitsu <kimitsu@mail.ru>
2024-04-15 21:17:16 +0300

    Реализован метод printCompareSumOfArrayHalves, печатающий результат сравнения двух половин массива

--- src/main/java/ru/otus/java/basic/hw2/MainApplication.java
+++ src/main/java/ru/otus/java/basic/hw2/MainApplication.java
@@ -29,6 +29,27 @@ public class MainApplication {
         }
     }
 
+    public static void printCompareSumOfArrayHalves(int[] array) {
+        if (array.length < 2) {
+            System.out.println("Ваш массив слишком мал");
+        } else {
+            long result = 0;
+            for (int i = 0; i < array.length / 2; i++) {
+                result += array[i];
+            }
+            for (int i = array.length / 2; i < array.length; i++) {
+                result -= array[i];
+            }
+            System.out.printf("Сумма элементов [0..%d] %s элементов [%d..%d]%n",
+                    array.length / 2 - 1,
+                    result > 0 ? "больше, чем сумма"
+                               : (result < 0 ? "меньше, чем сумма"
+                                             : "равна сумме"),
+                    array.length / 2,
+                    array.length - 1);
+        }
+    }
+
     public static void main(String[] args) {
         printNumberAndString(5, "Hello...");
         printSumOfElementsGreaterThanFive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}); // Must print 40
@@ -37,5 +58,6 @@ public class MainApplication {
         printSumOfElementsGreaterThanFive(testArray); // Must print 100
         incrementArray(testArray, 10);
         printSumOfElementsGreaterThanFive(testArray); // Must print 200
+        printCompareSumOfArrayHalves(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     }
 }
