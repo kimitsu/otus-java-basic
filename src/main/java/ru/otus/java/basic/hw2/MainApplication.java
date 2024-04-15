4f5c73f7aae1142ed6e3ef9a9c1cc14c8b4fc908
kimitsu <kimitsu@mail.ru>
2024-04-15 20:47:09 +0300

    Реализован метод printSumOfElementsGreaterThanFive, печатающий сумму элементов массива, которые больше 5

--- src/main/java/ru/otus/java/basic/hw2/MainApplication.java
+++ src/main/java/ru/otus/java/basic/hw2/MainApplication.java
@@ -7,7 +7,18 @@ public class MainApplication {
         }
     }
 
+    public static void printSumOfElementsGreaterThanFive(int[] array) {
+        long result = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 5) {
+                result += array[i];
+            }
+        }
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
         printNumberAndString(5, "Hello...");
+        printSumOfElementsGreaterThanFive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     }
 }
