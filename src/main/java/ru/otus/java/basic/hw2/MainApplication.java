19ca4a2bff6db1758e1177f17dd94901e8b37a4a
kimitsu <kimitsu@mail.ru>
2024-04-15 20:54:25 +0300

    Реализован метод fillArray, заполняющий массив указанным значением

--- src/main/java/ru/otus/java/basic/hw2/MainApplication.java
+++ src/main/java/ru/otus/java/basic/hw2/MainApplication.java
@@ -17,8 +17,17 @@ public class MainApplication {
         System.out.println(result);
     }
 
+    public static void fillArray(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+
     public static void main(String[] args) {
         printNumberAndString(5, "Hello...");
-        printSumOfElementsGreaterThanFive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        printSumOfElementsGreaterThanFive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}); // Must print 40
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        fillArray(testArray, 10);
+        printSumOfElementsGreaterThanFive(testArray); // Must print 100
     }
 }
