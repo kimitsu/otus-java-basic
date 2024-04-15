fd253835b3cb7f6103cbf80f334edec01c98d9de
kimitsu <kimitsu@mail.ru>
2024-04-15 20:38:32 +0300

    Реализован метод printNumberAndString, печатающий строку несколько раз

--- src/main/java/ru/otus/java/basic/hw2/MainApplication.java
+++ src/main/java/ru/otus/java/basic/hw2/MainApplication.java
@@ -1,4 +1,13 @@
 package ru.otus.java.basic.hw2;
 
 public class MainApplication {
+    public static void printNumberAndString(int number, String string) {
+        for (int i = 0; i < number; i++) {
+            System.out.println(string);
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumberAndString(5, "Hello...");
+    }
 }
