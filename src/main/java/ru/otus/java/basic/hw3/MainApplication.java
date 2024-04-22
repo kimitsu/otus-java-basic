7f32b6e76cb83ac76910bae9989e6f6ef9ce868b
kimitsu <kimitsu@mail.ru>
2024-04-22 20:31:35 +0300

    Реализован метод printSquare

--- src/main/java/ru/otus/java/basic/hw3/MainApplication.java
+++ src/main/java/ru/otus/java/basic/hw3/MainApplication.java
@@ -18,4 +18,32 @@ public class MainApplication {
         }
         return result;
     }
+
+    /**
+     * Печатает ребра квадрата из символов "*" в консоль
+     * и заполняет внутренность квадрата пробелами
+     *
+     * @param size Размер квадрата в символах
+     */
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j == 0 || j == size - 1 || i == 0 || i == size - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfPositiveElements(new int[][]{{0, 1, 2, 3}, {-1, -2, 3}, {}, {10, 20, 30, -40}}));
+
+        printSquare(10);
+
+    }
+
 }
